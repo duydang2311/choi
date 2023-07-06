19545dabba187ee8f275cdcc79db6b859bf747a5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-07-07 03:09:34 +0700

    feat: implement login

--- src/routes/[[lang=lang]]/login/+page.ts
+++ src/routes/[[lang=lang]]/login/+page.ts
@@ -1,8 +1,19 @@
+import { writable } from 'svelte/store';
+
+const form = writable({ name: '', password: '' });
+
 export const prerender = false;
 export const ssr = false;
 
 export const entries = () => [
 	{
 		lang: 'en'
+	},
+	{
+		lang: 'vi'
 	}
 ];
+
+export const load = () => {
+	return { form };
+};
